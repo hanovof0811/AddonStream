aa3c8cb234caf7a7a51456e066c32b0454601ad2
HaiGH-Space <nthai0932169055@gmail.com>
2024-08-29 14:22:08 +0700

    init

--- ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
+++ ExampleProvider/src/main/kotlin/com/example/ExampleProvider.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.MainAPI
 import com.lagradost.cloudstream3.SearchResponse
 
 class ExampleProvider(val plugin: TestPlugin) : MainAPI() { // all providers must be an intstance of MainAPI
-    override var mainUrl = "https://example.com/" 
+    override var mainUrl = "https://example.xx/"
     override var name = "Example provider"
     override val supportedTypes = setOf(TvType.Movie)
 
